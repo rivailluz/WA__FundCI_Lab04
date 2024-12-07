38d822e954992d8ceacc1516c136a9816f19af80
rivailluz <rivailluz@gmail.com>
2024-12-06 22:04:55 -0400

    fix: correção do lint

--- app.js
+++ app.js
@@ -1,12 +1,11 @@
-
-
-const express = require('express');
+/* eslint-env node */
+const express = require("express");
 const app = express();
 
-app.get('/', (req, res) => {
-  res.send('Bem vindos ao WebAcademy!!');
+app.get("/", (req, res) => {
+  res.send("Bem vindos ao WebAcademy!!");
 });
 
 app.listen(8080, () => {
-  console.log('Rodando na porta 8080');
+  console.log("Rodando na porta 8080");
 });
